a790382df2010dcf751be4becfcba2b46500aba2
Raj Kumar <raaz.kumar015@gmail.com>
2022-10-09 15:48:54 +0530

    intial code setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import code from './codesnip/code';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Extension
+     <ul>
+      {code.map(c => <li key={c.id}>{c.shortcode} || {c.code } </li>)}
+     </ul>
+    
     </div>
   );
 }
---
+++ src/codesnip/code.js
@@ -0,0 +1,14 @@
+const code = [
+    {
+        id:1,
+        shortcode:"Short Code",
+        code :'Code'
+    },
+    {
+        id:2,
+        shortcode:"Short Code 2",
+        code :'Code 2'
+    }
+]
+
+export default code
